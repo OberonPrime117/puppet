835776e7e6f7337914fdabfe061232a657194a1f
Aditya Tayade <71881484+OberonPrime117@users.noreply.github.com>
2024-01-21 00:43:48 +0530

    more changes to come !!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # puppet
 Scrape websites that are available on the surface web while remaining anonymous with Tor
+
+# KEY POINTS
+1. Puppet is restricting itself to only a single domain currently
+    - If you are removing single domain restriction then blacklist cloudflare websites
+2. tor.exe will be required to be running for this
---
+++ index.js
@@ -0,0 +1,100 @@
+const puppeteer = require('puppeteer');
+const mongoose = require('mongoose');
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/example', {useNewUrlParser: true});
+
+const pageSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    unique: true
+  },
+  html: String,
+  text: String,
+  title: String  
+});
+
+const Page = mongoose.model('Page', pageSchema);
+
+const MAX_PARALLEL_LINKS = 5;
+let urls = ['https://example.com'];
+
+async function crawl() {
+
+  const browser = await puppeteer.launch({
+    args: ["--proxy-server=socks5://127.0.0.1:9050"]
+  });  
+
+  const pages = await Promise.all([
+    browser.newPage(),
+    browser.newPage(),
+    browser.newPage(), 
+    browser.newPage(),
+    browser.newPage(),
+  ]);
+
+  while(urls.length > 0) {
+
+    let promises = [];
+
+    for(let i = 0; i < MAX_PARALLEL_LINKS; i++) {
+    
+      const currentUrl = urls.pop();
+
+      if(!currentUrl) {
+        break;
+      }
+
+      promises.push(crawlPage(pages[i], currentUrl));
+    }
+
+    await Promise.all(promises);
+  
+  }
+
+  browser.close();
+
+}
+
+
+
+async function crawlPage(page, currentUrl) {
+
+  const existing = await Page.findOne({url: currentUrl}).exec();
+  if (existing) {
+    console.log(`Skipping already saved URL: ${currentUrl}`);
+    return;
+  }
+
+  await page.goto(currentUrl, {'timeout': 180000});
+
+  // Extract info  
+  const html = await page.content();
+  const text = await page.evaluate(() => document.body.innerText);
+  const title = await page.title();
+
+  await new Page({ 
+     url: currentUrl,
+     html: html,
+     text: text,
+     title: title
+   }).save();
+
+   console.log(currentUrl);
+
+  const links = await page.evaluate(() => {
+
+    const isBBGLink = link => {
+      return link.includes('example.com') || link.includes('www.example.com');
+    }
+    
+    const anchors = document.querySelectorAll('a'); 
+    return [...anchors]
+    .map(anchor => anchor.href)
+    .filter(href => isBBGLink(href));;
+  });
+  
+  urls.push(...links);  
+}
+
+crawl();
